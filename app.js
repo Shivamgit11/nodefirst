384e860df85f1f441e5b8a5abf3e4f36549fb074
ShivamRauniyaar <145185374+ShivamRauniyaar@users.noreply.github.com>
2024-10-11 03:11:29 +0530

    showing multiple default based on the route name like /home , /about,/node,

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const http = require('http');
 
-const server =  http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
-    // process.exit()
-    res.setHeader("Content-Type", 'text/html')
-    res.write('<html>');
-    res.write('<head><title>My  First Page</title></head>');
-    res.write('<body><h1>Hellow from my node js server</h1></body>')
-    res.write('</html>')
-    res.end()
-})
 
-server.listen(3000);
+    res.setHeader("Content-Type", 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    
+    if (req.url === '/home') {
+        res.write('<body><h1>Welcome home</h1></body>');
+    } else if (req.url === '/about') {
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+    } else if (req.url === '/node') {
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    } else {
+        res.write('<body><h1>Hello from my Node.js server</h1></body>');
+    }
+    
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
