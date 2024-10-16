ae2358fe88e6594feea323d46a0a426e01cf3520
ShivamRauniyaar <145185374+ShivamRauniyaar@users.noreply.github.com>
2024-10-17 01:55:34 +0530

    added text into the file based on the condition

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    // console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+
+    if(url === "/"){
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>')
+
+    res.write('</html>');
+    return res.end();
+
+    }
+
+    if(url === '/message' && method === "POST" ){
+        const body= [];
+        req.on('data', (chunks)  => {
+            console.log(chunks)
+            body.push(chunks)
+        })  //in use to listen some of event 
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1]
+            console.log(parsedBody)
+        fs.writeFileSync('message.txt', message);
+
+
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        return res.end()
+    }
 
     res.setHeader("Content-Type", 'text/html');
     res.write('<html>');
@@ -14,7 +46,7 @@ const server = http.createServer((req, res) => {
     } else if (req.url === '/node') {
         res.write('<body><h1>Welcome to my Node Js project</h1></body>');
     } else {
-        res.write('<body><h1>Hello from my Node.js server</h1></body>');
+        // res.write('<body><h1>Hello from my Node.js server</h1></body>');
     }
     
     res.write('</html>');
